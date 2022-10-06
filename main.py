408940b95a31c1570d874f6543fcafd127ca362a
RicardoMFC <113980950+RicardoMFC@users.noreply.github.com>
2022-10-06 09:13:23 +0000

    Añadir diagrama de flujo

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+numero=random.randint(0,100)
+i=0
+intento = int(input("Escriba un numero\n"))
+
+while True:
+  intento = int(input("Escriba un numero\n"))
+  if 
+if 0>intento or intento>100:
+  intento = int(input("Escriba un numero\n"))
+
+  
