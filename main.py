85289eae059f273163877d7c93f4ebe9e6f50a3c
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-10 17:58:02 +0200

    y

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import random
 numero=random.randint(0,100)
-def introducir_num(x)
-i=0 #def 
-intento = int(input("Escriba un numero\n"))
 
-while True:    
+def introducir_num():
+  intento = int(input("Escriba un numero\n"))
   while intento<0 or intento>100:
     intento = int(input("Escriba un numero\n"))
-
-  if intento==numero:
-    print("Ha acertado el numero\n")
-    break
-  elif intento>numero:
-    print("El numero es menor\n")
-  else:
-    print("El numero es mayor\n")
-  intento=-1
-  i=i+1
-
-print(i)
-#if name==
-
+  return intento
   
+def adivinar_numero(intento):
+  while True:
+    introducir_num()
+    if intento==numero:
+      print("Ha acertado el numero\n")
+      break
+    elif intento>numero:
+      print("El numero es menor\n")
+    else:
+      print("El numero es mayor\n")
+
+
+
+
   
 
 
