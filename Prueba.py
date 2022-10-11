33d72b89d9aa4e7d6057c297bbfe6a8f0ca11c30
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-11 11:36:31 +0200

    r

--- Prueba.py
+++ Prueba.py
@@ -1,21 +1,34 @@
 import random
 numero=random.randint(0,100)
-i=0
-intento = int(input("Escriba un numero\n"))
-def acertar_num:
-  while True:    
-  while intento<0 or intento>100:
-    intento = int(input("Escriba un numero\n"))
+import sys
 
-  if intento==numero:
-    print("Ha acertado el numero\n")
-    break
-  elif intento>numero:
-    print("El numero es menor\n")
+def introducir_num():
+  try:
+    intento = int(input("Escriba un numero\n"))
+  except:
+    pass
   else:
-    print("El numero es mayor\n")
-  intento=-1
-  
+     return intento
+  sys.exit()
+
+def adivinar_numero():
+  l_sup=100
+  l_inf=0
+  i=0
+  while True:
+    i+=1
+    x=introducir_num()
+    if x==numero:
+      print("Ha acertado el numero con {} intentos\n".format(i))
+      break
+    elif x>numero:
+      l_sup=x
+      print("Rango de valores ({}, {})\n".format(l_inf,l_sup))
+    else:
+      l_inf=x
+      print("Rango de valores ({}, {})\n".format(l_inf, l_sup))
+adivinar_numero()
+
 
   
   
