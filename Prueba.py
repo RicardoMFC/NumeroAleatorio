eeccafa55ca0b217fb32605ad32cf0f444d008af
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-13 21:11:57 +0200

    r

--- Prueba.py
+++ Prueba.py
@@ -1,19 +1,18 @@
 import random
 import sys
-
-def indicar_lim_inf():
-  limite_inferior= int (input("Escriba el limite inferior\n"))
-  return limite_inferior
-
-def indicar_lim_sup():
-  limite_superior= int (input("Escriba el limite superior\n"))
-  return limite_superior
+def main():
+    l_inf, l_sup = indicarLimites()
   
-def comparacion_limites(x,y):
-  val=0
-  if x<y:
-    val=1
-  return val
+
+
+
+  
+def indicarLimites():
+  while True:
+    limite_inferior= int (input("Escriba el limite inferior\n"))
+    limite_superior= int (input("Escriba el limite superior\n"))
+    if limite_inferior<limite_superior:
+      return limite_inferior, limite_superior
 
 def introducir_num(a,b):
   try:
@@ -27,30 +26,24 @@ def introducir_num(a,b):
   sys.exit()
 
 def adivinar_numero():
-  l_sup = indicar_lim_sup()
-  l_inf = indicar_lim_inf()
-  try:
-    z=comparacion_limites(l_inf,l_sup)
-    while z==0:
-      l_sup = indicar_lim_sup()
-      l_inf = indicar_lim_inf()
-      z=comparacion_limites(l_inf,l_sup)
-
-  except:
-    pass
+  l_inf, l_sup = indicarLimites()
   numero=random.randint(l_inf,l_sup)
-
   numero_intentos=0
+  
   while True:
     numero_intentos+=1
     x=introducir_num(l_inf,l_sup)
     if x==numero:
-      print("Ha acertado el numero con {} intentos\n".format(numero_intentos))
-      break
+      if numero_intentos==1:
+        print("Ha acertado el numero con {} intento\n".format(numero_intentos))
+        break
+      else:
+        print("Ha acertado el numero con {} intento\n".format(numero_intentos))
+        break
     elif x>numero:
-      l_sup=x
-      print("Rango de valores ({}, {})\n".format(l_inf,l_sup))
+      l_sup=x-1
+      print("Rango de valores [{}, {}]\n".format(l_inf,l_sup))
     else:
-      l_inf=x
-      print("Rango de valores ({}, {})\n".format(l_inf, l_sup))
+      l_inf=x+1
+      print("Rango de valores [{}, {}]\n".format(l_inf, l_sup))
 adivinar_numero()
