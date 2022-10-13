ded9c4bc49eaf72003a9a38ee642808761bb1894
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-13 23:08:52 +0200

    t

--- Prueba3.py
+++ Prueba3.py
@@ -1,12 +1,27 @@
 import random
 import sys
 
-def indicarLimites():
-  while True:
-    limite_inferior = int (input("Escriba el limite inferior\n"))
-    limite_superior = int (input("Escriba el limite superior\n"))
-    if limite_inferior < limite_superior:
-      return limite_inferior, limite_superior
+def indicar_dificultad():
+    limite_inferior=0
+    try:
+        w = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
+        while w<1 or w>4:
+            w = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
+    except:
+        pass
+    if w=1:
+        limite_superior=100
+    elif w=4:
+        limite_superior=1000000000000
+    else:
+        limite_superior=1000^(w-1)
+    return limite_inferior, limite_superior
+
+def ayuda():
+    c = input ("Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n")
+    while (c!=0 or c!=1):
+        c = input ("Error. Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n")
+    return c
 
 def introducir_num(a,b):
   try:
@@ -19,7 +34,7 @@ def introducir_num(a,b):
     return intento
   sys.exit()
 
-def adivinar_numero(numero, l_inf, l_sup):
+def adivinar_numero_con_ayuda(numero, l_inf, l_sup):
     contador=0
     while True:
         contador+=1
@@ -32,10 +47,16 @@ def adivinar_numero(numero, l_inf, l_sup):
         else:
             l_inf=x+1
             print("Rango de valores [{}, {}]\n".format(l_inf, l_sup))
+
+def adivinar_numero_sin_ayuda ():
     
 def main():
-    l_inf, l_sup = indicarLimites()
+    l_inf, l_sup = indicar_dificultad()  
     numero=random.randint(l_inf,l_sup)
+    u = ayuda()
+    if ayuda == 1:
+        adivinar_numero_con_ayuda(numero, )
+ 
     nºintentos = adivinar_numero(numero, l_inf, l_sup)
     print("Ha acertado con {} intentos\n".format(nºintentos))
 main()
