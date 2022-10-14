cd9055c2f4edcb07b5d3b65a20512720c37ecb31
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-14 21:16:53 +0200

    r

--- Prueba3.py
+++ Prueba3.py
@@ -1,62 +1,26 @@
 import random
-import sys
+numero=random.randint(0,100)
 
-def indicar_dificultad():
-    limite_inferior=0
-    try:
-        w = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
-        while w<1 or w>4:
-            w = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
-    except:
-        pass
-    if w=1:
-        limite_superior=100
-    elif w=4:
-        limite_superior=1000000000000
+def introducir_num():
+  intento = int(input("Escriba un numero\n"))
+  while intento<0 or intento>100:
+    intento = int(input("Escriba otro numero\n"))
+  return intento
+
+def adivinar_numero():
+  l_sup=100
+  l_inf=0
+  i=0
+  while True:
+    i+=1
+    x=introducir_num()
+    if x==numero:
+      print("Ha acertado el numero con {} intentos\n".format(i))
+      break
+    elif x>numero:
+      l_sup=x
+      print("Rango de valores ({}, {})\n".format(l_inf,l_sup))
     else:
-        limite_superior=1000^(w-1)
-    return limite_inferior, limite_superior
-
-def ayuda():
-    c = input ("Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n")
-    while (c!=0 or c!=1):
-        c = input ("Error. Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n")
-    return c
-
-def introducir_num(a,b):
-  try:
-    intento = int(input("Escriba un numero\n"))
-    while intento<a or intento>b:
-      intento = int(input("Escriba otro numero\n"))
-  except:
-    pass
-  else:
-    return intento
-  sys.exit()
-
-def adivinar_numero_con_ayuda(numero, l_inf, l_sup):
-    contador=0
-    while True:
-        contador+=1
-        x=introducir_num(l_inf, l_sup)
-        if x==numero:
-            return contador
-        elif x>numero:
-            l_sup=x-1
-            print("Rango de valores [{}, {}]\n".format(l_inf,l_sup))
-        else:
-            l_inf=x+1
-            print("Rango de valores [{}, {}]\n".format(l_inf, l_sup))
-
-def adivinar_numero_sin_ayuda ():
-    
-def main():
-    l_inf, l_sup = indicar_dificultad()  
-    numero=random.randint(l_inf,l_sup)
-    u = ayuda()
-    if ayuda == 1:
-        adivinar_numero_con_ayuda(numero, )
- 
-    nºintentos = adivinar_numero(numero, l_inf, l_sup)
-    print("Ha acertado con {} intentos\n".format(nºintentos))
-main()
+      l_inf=x
+      print("Rango de valores ({}, {})\n".format(l_inf, l_sup))
+adivinar_numero()
--- main.py
+++ main.py
@@ -1,26 +1,91 @@
 import random
-numero=random.randint(0,100)
+import sys
 
-def introducir_num():
-  intento = int(input("Escriba un numero\n"))
-  while intento<0 or intento>100:
-    intento = int(input("Escriba otro numero\n"))
-  return intento
-
-def adivinar_numero():
-  l_sup=100
-  l_inf=0
-  i=0
-  while True:
-    i+=1
-    x=introducir_num()
-    if x==numero:
-      print("Ha acertado el numero con {} intentos\n".format(i))
-      break
-    elif x>numero:
-      l_sup=x
-      print("Rango de valores ({}, {})\n".format(l_inf,l_sup))
+def indicar_dificultad():
+    limite_inferior=0
+    try:
+        dificultad = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
+        while dificultad<1 or dificultad>4:
+            dificultad = int (input("Indicar el nivel de dificultad\nNivel simple [0, 100] pulse 1\nNivel Intermedio[0, 1.000] pulse 2\nNivel avanzado[0, 1.000.000] pulse 3\nNivel experto[0, 1.000.000.000.000] pulse 4\n"))
+    except:
+        pass
+    if dificultad==1:
+        limite_superior=100
+    elif dificultad==2:
+      limite_superior=1000
+    elif dificultad==3:
+      limite_superior=1000000
     else:
-      l_inf=x
-      print("Rango de valores ({}, {})\n".format(l_inf, l_sup))
-adivinar_numero()
+        limite_superior=1000000000000
+
+    return limite_inferior, limite_superior, dificultad
+
+def ayuda():
+    valor_ayuda = int (input ("Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n"))
+    while (valor_ayuda!=0 and valor_ayuda!=1):
+        valor_ayuda = int( input ("Error. Indicar con un 1 si quiere ayuda de rangos, sino escriba un 0\n"))
+    return valor_ayuda
+
+def numero_maximo_intentos(dificultad):
+    #x son el numero de intentos permitidos
+    if dificultad==1:
+        intentos=10
+    elif dificultad==2:
+        intentos=50
+    elif dificultad==3:
+        intentos=100
+    else:
+        intentos=1000
+    return intentos
+    
+        
+def introducir_num(l_inf,l_sup):
+  try:
+    intento = int(input("Escriba un numero\n"))
+    while intento<l_inf or intento>l_sup:
+      intento = int(input("Escriba otro numero\n"))
+  except:
+    pass
+  else:
+    return intento
+  sys.exit()
+
+def adivinar_numero(numero, l_inf, l_sup, valor_ayuda, intentos_permitidos):
+    contador_intentos=0
+    while True:
+        contador_intentos+=1
+        intento=introducir_num(l_inf, l_sup)
+
+        if intento==numero:
+            return contador_intentos
+        
+        else:
+            if intento>numero:
+                print("\nEl numero es menor")
+                l_sup=intento-1
+            else:
+                print ("\nEl numero es mayor")
+                l_inf=intento+1
+            
+            if valor_ayuda==1:
+                    ayuda_rangos(l_inf, l_sup)
+                
+        if contador_intentos==intentos_permitidos:
+            return 0
+
+def ayuda_rangos (l_inf, l_sup):
+    print("Rango de valores [{}, {}]\n".format(l_inf,l_sup))
+
+def jugar():
+
+    l_inf, l_sup, dificultad = indicar_dificultad()  
+    numero=random.randint(l_inf, l_sup)
+    valor_ayuda = ayuda()
+    intentos_permitidos = numero_maximo_intentos(dificultad)
+    nºintentos = adivinar_numero (numero, l_inf, l_sup, valor_ayuda, intentos_permitidos)
+
+    if nºintentos==0:
+        print ("Ha utilizado el número máximo de oportunidades\n")
+    else:
+        print("Ha acertado con {} intentos\n".format(nºintentos))  
+jugar()
