f6c53f6f6d02352bdca8c0af3db90606e8934158
RicardoMFC <rich10fernandezconde@gmail.com>
2022-10-16 20:10:18 +0200

    r

--- Prueba3.py
+++ Prueba3.py
@@ -1,8 +1,26 @@
 import random
 import sys
+from usuario import IA, introducir_num
 
-from main import introducir_num
 
+def desea_jugar():
+    valor=0
+    palabra=input("Si quiere jugar escriba si, sino quiere escriba cualquier cosa\n")
+    palabra=palabra.lower()
+    if palabra=='si':
+        valor=1
+    return valor
+
+def fun_usuario ():
+  try:
+    valor = int (input("Escriba un 1 si quiere usar la IA, si quiere jugar usted escriba un 0\n"))
+    while valor!=0 and valor!=1:
+      valor = int (input("Escriba un 1 si quiere usar la IA, si quiere jugar usted escriba un 0\n"))
+
+  except:
+    pass
+  else:
+    return valor
 
 def indicar_dificultad():
     limite_inferior=0
@@ -46,13 +64,12 @@ def numero_maximo_intentos(dificultad):
     return intentos
     
 
-
-def adivinar_numero(numero, l_inf, l_sup, valor_ayuda, intentos_permitidos):
+def adivinar_numero(numero, l_inf, l_sup, valor_ayuda, intentos_permitidos, usuario):
     contador_intentos=0
     while True:
         contador_intentos+=1
         if usuario==1:
-          intento=IA(l_inf, l_sup)
+          intento = IA(l_inf, l_sup)
         else:
           intento=introducir_num(l_inf, l_sup)
 
@@ -73,32 +90,34 @@ def adivinar_numero(numero, l_inf, l_sup, valor_ayuda, intentos_permitidos):
         if contador_intentos==intentos_permitidos:
             return 0
 
-def usuario ():
-  try:
-    valor = int (input("Escriba un 1 si quiere usar la IA, si quiere jugar usted escriba un 0"))
-    while valor !=1 and valor!=1:
-      valor = int (input("Escriba un 1 si quiere usar la IA, si quiere jugar usted escriba un 0"))
-    if usuario==0:
-      import introducir_num from persona 
-    else:
-      from IA import IA()
-    
-  except:
-    pass
-  else:
-    return valor
+
 
 def jugar():
+    contador=0
+    while True:
+        valor = desea_jugar()
+        if valor==0:
+            break
 
-    l_inf, l_sup, dificultad = indicar_dificultad()  
-    numero=random.randint(l_inf, l_sup)
-    valor_ayuda = ayuda()
-    intentos_permitidos = numero_maximo_intentos(dificultad)
-    usuario = usuario()
-    nºintentos = adivinar_numero (numero, l_inf, l_sup, valor_ayuda, intentos_permitidos, usuario)
+        l_inf, l_sup, dificultad = indicar_dificultad()  
+        numero=random.randint(l_inf, l_sup)
+        valor_ayuda = ayuda()
+        intentos_permitidos = numero_maximo_intentos(dificultad)
+        usuario = fun_usuario()
+        nºintentos = adivinar_numero (numero, l_inf, l_sup, valor_ayuda, intentos_permitidos, usuario)
 
-    if nºintentos==0:
-        print ("Ha utilizado el número máximo de oportunidades\n")
+        if nºintentos==0:
+            print ("Ha utilizado el número máximo de oportunidades\n")
+        else:
+            print ("Ha acertado con {} intentos\n".format(nºintentos))
+        contador+=1  
+    if contador==1:
+        print ("Ha jugado {} vez\n".format(contador))
     else:
-        print("Ha acertado con {} intentos\n".format(nºintentos))  
-jugar()
+        print ("Ha jugado {} veces\n".format(contador))
+
+if __name__=='__main__':
+    print ("Es este modulo\n")
+    jugar()
+else:
+    print ("Se ha importado el modulo\n")
--- usuario.py
+++ usuario.py
@@ -1,3 +1,8 @@
+def IA(l_inf,l_sup):
+    intento = int ((l_sup+l_inf)/2)
+    print(intento)
+    return intento
+
 def introducir_num(l_inf,l_sup):
   try:
     intento = int(input("Escriba un numero\n"))
